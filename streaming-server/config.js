b94a9ab307e1f905ebb927f7ab4c80de6913b45c
mqlstam <118191161+mqlstam@users.noreply.github.com>
2024-06-06 13:05:32 +0200

    Node Streaming and Web Server

---
+++ streaming-server/config.js
@@ -0,0 +1,33 @@
+const config = {
+  logType: 2,
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*',
+    mediaroot: './media'
+  },
+  https: {
+    port: 8443,
+    key: './cert/key.pem',
+    cert: './cert/cert.pem'
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
+};
+module.exports = config;
---
+++ streaming-server/server.js
@@ -0,0 +1,4 @@
+const NodeMediaServer = require('node-media-server');
+const config = require('./config');
+const nms = new NodeMediaServer(config);
+nms.run();
